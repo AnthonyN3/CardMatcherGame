433b295bacd0a94e1741c35e9a7791f4fcf6b88a
Anthony Nguyen <46362634+AnthonyN3@users.noreply.github.com>
2019-03-06 01:21:41 -0500

    Creating a Main Menu

---
+++ nguye145_MemoryGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {   
+        //Main menu is set as the first index (aka 0) in the build, and the game is set to index 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);        //Increment to the next index in the build scene
+    }
+
+    public void Exit()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit(); //NOTE: will not quit while in Unity Editor (only quits after buildt game)
+    }
+
+    public void UniqueCards(int input)
+    {
+        PlayerData.numOfCards = input;
+        Debug.Log(PlayerData.numOfCards);
+    }
+
+}
---
+++ nguye145_MemoryGame/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData 
+{   
+    //The number of unique cards is defaulted to 6
+    public static int numOfCards = 6;
+}
