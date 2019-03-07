3d62291376959fb3f1b372360a62235e46282d4a
Anthony Nguyen <46362634+AnthonyN3@users.noreply.github.com>
2019-03-07 15:22:17 -0500

    Adding Logic of card matching

---
+++ nguye145_MemoryGame/Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public int id;
+
+    public void OnClick()
+    {
+        Debug.Log("WE");
+        if(gameObject.transform.parent.GetComponent<PopulateGrid>().isPressed)
+        {
+            Debug.Log("OKAY");
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.parent.GetComponent<PopulateGrid>().TEST(id,gameObject);
+            gameObject.GetComponent<Button>().enabled = false;
+        }
+    }
+
+}
--- nguye145_MemoryGame/Assets/Scripts/PopulateGrid.cs
+++ nguye145_MemoryGame/Assets/Scripts/PopulateGrid.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class PopulateGrid : MonoBehaviour
 {
     public GameObject cards;
-    public int numOfCards;
+    [SerializeField]
+    private Sprite[] Images;
+    private int[] idNumbers = {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9};
+    private int pressed = 0;
+    private int cardOne = -1, cardTwo = -2;
+    private GameObject CardOne, CardTwo;
+    public bool isPressed = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        isPressed = true;
         populate();
     }
 
@@ -21,10 +31,74 @@ public class PopulateGrid : MonoBehaviour
     {
         GameObject newObj;
 
-        for(int i = 0; i < numOfCards ; i++)
+        for(int i = 0; i < PlayerData.numOfCards*2 ; i++)
         {
             newObj = (GameObject)Instantiate(cards, transform);
+            //newObj.GetComponent<SpriteRenderer>().sprite = Images[i];
+            //newObj.transform.GetChild(0).image.sprite = Images[1];
+            newObj.GetComponent<Card>().id = idNumbers[i];
+            newObj.transform.GetChild(0).GetComponent<Image>().sprite = Images[newObj.GetComponent<Card>().id];
+        }
+            
+    }
+
+    public void TEST(int clickCardsId, GameObject thisCard)
+    {
+        
+        pressed++;
+
+        if(pressed == 1)
+        {
+            cardOne = clickCardsId;
+            CardOne = thisCard;
+
+
+            return;
+        }
+        else if (pressed == 2)
+        {
+            cardTwo = clickCardsId;
+            CardTwo = thisCard;
+        }
+
+        if(isPressed)
+        {     
+            pressed = 0; 
+            isPressed = false;    
+            
+            Invoke("DoTheyMatch", 1.5f);
         }
 
     }
+
+    private void DoTheyMatch()
+    {
+        if(cardOne == cardTwo)
+        {
+            cardOne = -1;
+            cardTwo = -2;
+            isPressed = false;
+            CardOne = null;
+            CardTwo = null;
+        }
+        else
+        {
+            CardOne.GetComponent<Button>().enabled = true;
+            CardTwo.GetComponent<Button>().enabled = true;
+            
+
+            CardOne.transform.GetChild(0).gameObject.SetActive(false);
+            CardTwo.transform.GetChild(0).gameObject.SetActive(false);
+
+            cardOne = -1;
+            cardTwo = -2;
+            CardOne = null;
+            CardTwo = null;
+            isPressed = false;
+        }
+
+        isPressed = true;
+    }
+
+
 }
